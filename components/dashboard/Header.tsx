9aaa700aab08ec3b33348cb279b52abac119a504
MarwanElAbhar <marwan@aklat.io>
2026-06-18 00:41:43 +0300

    Add Header component for dashboard

---
+++ components/dashboard/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="flex items-center justify-between h-24 p-6">
+            <h1 className="text-2xl font-bold text-primary">Proctor Dashboard</h1>
+            <h2 className="text-lg font-medium text-secondary">User</h2>
+        </header>
+    )
+}
