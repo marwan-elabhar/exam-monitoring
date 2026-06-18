558e450728d3f02b221589a83662a6a87bd7c977
MarwanElAbhar <marwan@aklat.io>
2026-06-18 14:29:56 +0300

    Register `activitySlice` in the Redux store to manage activity state

--- store/index.ts
+++ store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import candidatesReducer from "./slices/candidatesSlice";
 import filtersReducer from "./slices/filtersSlice";
 import uiReducer from "./slices/uiSlice";
+import activityReducer from "./slices/activitySlice";
 
 export const store = configureStore({
   reducer: {
     candidates: candidatesReducer,
     filters: filtersReducer,
     ui: uiReducer,
+    activity: activityReducer,
   },
 });
 
